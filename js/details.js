13e5d7d97181ac027fb734fcf01d782dce57f207
OmarBaltaji <omr.baltaji@gmail.com>
2021-03-23 18:18:45 +0200

    fixed details open/close function

--- js/details.js
+++ js/details.js
@@ -3,7 +3,7 @@ const details = document.querySelectorAll("details");
 details.forEach(targetDetail => {
     targetDetail.addEventListener('click', () => {
         details.forEach(detail => {
-            if (detail !== targetDetail) {
+            if (detail !== targetDetail) { // Keeps the detail element that was clicked open, while closing all others
                 detail.removeAttribute('open');
             }
         })
